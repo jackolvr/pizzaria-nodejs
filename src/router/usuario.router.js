00494c7119680278834b4d0d8bd3098b7923a74e
Jack Oliveira <jadsonolvr@gmail.com>
2025-05-06 00:30:40 -0300

    feat: usuario controller

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -3,17 +3,17 @@ const usuarioController = require("../controller/usuario.controller");
 
 const router = express.Router();
 
-router.get('/findById/:id');
-router.get('/findAll');
+router.get('/findById/:id', usuarioController.findUserByIdController);
+router.get('/findAll', usuarioController.findAllUsersController);
 
-router.post('/create');
-router.post('/addAddress/:id');
-router.post('/addFavProduct:/:id');
+router.post('/create', usuarioController.createUserController);
+router.post('/addAddress/:id', usuarioController.addAddressController);
+router.post('/addFavProduct/:id', usuarioController.addFavProductController);
 
-router.put('/update/:id');
+router.put('/update/:id', usuarioController.updateUserController);
 
-router.delete('/remove/:id');
-router.delete('/removeAddress');
-router.delete('removeFavProduct');
+router.delete('/remove/:id', usuarioController.removeUserController);
+router.delete('/removeAddress', usuarioController.removeAddressController);
+router.delete('/removeFavProduct', usuarioController.removeFavProductController);
 
 module.exports = router;
